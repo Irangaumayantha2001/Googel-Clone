b1084d400b0a6b6072a7d6e29c6a772210dda5d0
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-06-15 11:55:01 +0530

    add home elements in home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import "./Home.css";
+import AppsIcon from '@mui/icons-material/Apps';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from "react-router-dom";
+import Search from "../components/Search";
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+    <div className="home_header">
+      <div className="home_headerLeft">
+        <Link to="/about">About</Link>
+        <Link to="/about">Store</Link>
+      </div>
+      <div className="home_headerRight">
+        {/*Link */}
+        <Link to="/about">Gmail</Link>
+        <Link to="/about">Images</Link>
+        <AppsIcon/>
+        <AccountCircleIcon/>
+      </div>
+    </div>
+    <div className="home_body">
+      <img src="	https://www.google.lk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt=""  />
+      <div className="home_inputContainer">
+      <Search/>
+      </div>
+    </div>
+  </div>
   )
 }
 
